f61fd5e1aa5d4368b57b826bc6ae9bc16c2c58d7
LincolnFellipe <lincolnfellipe171@gmail.com>
2023-02-24 21:40:22 -0300

    Add project files.

---
+++ Controllers/PlayerStatsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using TesteGPT.API.Models;
+
+namespace TesteGPT.API.Controllers
+{
+    [ApiController]
+    [Route("api/player-stats")]
+    public class PlayerStatsController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public PlayerStatsController(HttpClient httpClient) {  
+        _httpClient = httpClient; 
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration) 
+        {
+            //Espaço para buscar a chave de configuração do token da API (openAI) de onde você tiver guardado.
+            //Por questões de segurança, ao subir para o git, não vou colocar minha chave, você pode conseguir uma em https://platform.openai.com/account/api-keys
+            var token = configuration.GetValue<string>("");           
+            //Montando o token de autorização
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Inicializando o modelo de entrada
+            var model = new ChatGPTInputModel(text);
+
+            //Serializo o modelo
+            var requestBody = JsonSerializer.Serialize(model);
+
+            //Inicio uma stringContent
+            var content = new StringContent(requestBody, Encoding.UTF8,"application/json");
+
+            //Realizo o post na api da openai
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/completions", content);
+
+            //Deserializo a resposta da API
+            var result = await response.Content.ReadFromJsonAsync<ChatGptViewModel>();
+
+            //Obtenho o primeiro elemento dentro do choices (Que é a resposta do chatGPT de fato)
+            var promptResponse = result.choices.First();
+
+            //Realizo o tratamento do response para ter uma visualização mais limpa da resposta
+            return Ok (promptResponse.text.Replace("/n", "").Replace("/t",""));
+        }
+    }
+}
---
+++ Models/ChatGptInputModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TesteGPT.API.Models
+{
+    public class ChatGPTInputModel
+    {
+        public ChatGPTInputModel(string prompt)
+        {
+            //Aqui nesta implementação, estou dando um contexto para o chatGPT compreender melhor o que eu desejo, nesse caso, estou solicitando a quantidade de gols do jogador.
+            //Poderia ser qualquer outra estatistica, como: Títulos, Lesões, etc.
+            this.prompt = $"Tell me amount of goals of soccer player: {prompt} ";
+            temperature = 0.2m;
+            max_tokens = 100;
+            model = "text-davinci-003";
+        }
+
+        public string model { get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public decimal temperature { get; set; }
+    }
+}
